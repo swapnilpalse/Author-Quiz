1ff209cc6f3719a7a665a245334c89a4ff75ad35
Palse,Swapnil <swapnil.palse@gmail.com>
2018-09-06 14:27:14 -0500

    Created the basic component and ensured that the app is working

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -2,20 +2,54 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './AuthorQuiz.css';
 import './bootstrap.min.css';
+
+function Hero(){
+  return(
+    <div className="row">
+      <div className="jumbotron col-10 offset-1">
+      <h1>Author Quiz</h1>
+      <p>Select a Book written by an author</p>
+      </div>
+    </div>
+  )
+}
+
+function Turn(){
+  return(<div></div>);
+}
+function Continue(){
+  return(<div></div>);
+}
+function Footer() {
+  return(
+    <div id="footer" className="row">
+      <div className="col-12">
+        <p className="text-muted credit">
+          All images are from <a href="https://commons.wikimedia.org/wiki/Main_Page">WikiMedia Commons</a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+
+
 class AuthorQuiz extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    <div className="container-fluid">
+    <Hero/>
+    <Turn/>
+    <Continue/>
+    <Footer/>
+    </div>
+  );
 }
+}
+
+/*
+
+*/
+
 
 export default AuthorQuiz;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import    from './AuthorQuiz';
+import  AuthorQuiz from './AuthorQuiz.js';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
